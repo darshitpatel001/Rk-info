8903f2e1c97faaa5c5e6762faf7bcd41a9848f64
darshitpatel <darshitakbari9999@gmail.com>
2023-06-28 23:41:27 -0700

    SAD

--- rk-info/src/component/contact/Update.js
+++ rk-info/src/component/contact/Update.js
@@ -11,7 +11,7 @@ export default function Update() {
                             <p className='social2'>Adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad.</p>
                         </div>
                         <div className="col-lg-6">
-                            <form action="https://formsubmit.co/rktechnology2022@gmail.com" method="POST" >
+                            <form action="https://formsubmit.co/darshitakbari9999@gmail.com" method="POST" >
                                 <input htmlFor="email" type="email" placeholder='Enter Your Email Address' name="email" />
                                 <input htmlFor="submit" type="submit" value="Subscribe" className='btn' />
                             </form>
--- rk-info/src/component/services/Updates.js
+++ rk-info/src/component/services/Updates.js
@@ -21,7 +21,6 @@ export default function Updates() {
                     </div>
                 </div>
             </div>
-            
         </>
     )
 }
