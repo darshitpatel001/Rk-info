39ba222c0f4bbb733339a8f4b1a2937881bb46fa
darshitpatel <darshitakbari9999@gmail.com>
2023-07-05 23:08:57 -0700

    fvgb

--- rk-info/src/component/home/Carousel.js
+++ rk-info/src/component/home/Carousel.js
@@ -12,54 +12,6 @@ export default function Carousel() {
     }, []);
     return (
         <>
-            {/* <div id="demo" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-indicators">
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="0" className="active"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
-                    <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
-                </div>
-                <div className="carousel-inner">
-                    <div className="carousel-item active">
-                        <img src={Carousel1} alt="Los Angeles" className="d-block" />
-                        <div className="carousel-container wow animate__backInLeft">
-                            <div className="container">
-                                <h2 className='demoo'>What Problems can <br />repair solve</h2>
-                                <p>RK is a neat, feature-rich landing page
-                                    template <br />designed to showcase your product or service in <br />style.</p>
-                                <a href="" className="btn-get-started animate_animated animate_fadeInUp scrollto">Visit Online Store</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={Carousel2} alt="Chicago" className="d-block" />
-                        <div className="carousel-container">
-                            <div className="container">
-                                <h2 className='demoo'>What Problems Can</h2>
-                                <p>RK is a neat, feature-rich landing page
-                                    template <br />designed to showcase your product or service in <br />style.</p>
-                                <a href="" className="btn-get-started animate_animated animate_fadeInUp scrollto">Visit Online Store</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item">
-                        <img src={Carousel3} alt="" className="d-block" />
-                        <div className="carousel-container">
-                            <div className="container">
-                                <h2 className='demoo'>What Problems Can</h2>
-                                <p>RK is a neat, feature-rich landing page
-                                    template <br />designed to showcase your product or service in <br />style.</p>
-                                <a href="" className="btn-get-started animate_animated animate_fadeInUp scrollto">Visit Online Store</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon"></span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
-                    <span className="carousel-control-next-icon"></span>
-                </button>
-            </div> */}
             <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
