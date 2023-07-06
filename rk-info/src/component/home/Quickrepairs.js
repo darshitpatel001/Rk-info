0ac4adf98d3887aa35b0c6f3a869a6aec3f88cf8
darshitpatel <darshitakbari9999@gmail.com>
2023-07-06 14:28:21 +0530

    sd

--- rk-info/src/component/home/Quickrepairs.js
+++ rk-info/src/component/home/Quickrepairs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Services1 from '../assets/img/Rectangle 14.png'
 import Services2 from '../assets/img/Rectangle 13.png'
 import Services3 from '../assets/img/Smartdoor.png'
+import right from '../assets/img/right.png'
 import { Link } from 'react-router-dom'
 
 export default function Quickrepairs() {
@@ -17,7 +18,7 @@ export default function Quickrepairs() {
                         <div className='px-2'>
                             <img
                                 className='service1 repire imgss wow animate__zoomInUp'
-                               data-wow-duration="1s"
+                                data-wow-duration="1s"
                                 src={Services1}
                                 alt=''
                             />
@@ -43,77 +44,26 @@ export default function Quickrepairs() {
                             <h1 className='quick1 pt-3 wow animate__backInLeft' data-wow-duration="1s">Our Services Save Your Time</h1>
                             <p className='quick2 wow animate__backInLeft' data-wow-duration="1s">Dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. </p>
                             <div className=' wow animate__backInLeft' data-wow-duration="2s">
-                                <div className="service mt-10">
-                                    <svg
-                                        viewBox="0 0 24 24"
-                                        style={{
-                                            width: "1.2000000000000002rem",
-                                            display: "inline-block",
-                                            height: "1.2000000000000002rem",
-                                        }}
-                                        role="presentation"
-                                        className=""
-                                        data-align="middle"
-                                    >
-                                        <path
-                                            d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
-                                            style={{ fill: "#FF7400" }}
-                                        ></path>
-                                    </svg>
-                                    <span>
-
+                                <div className=''>
+                                    <div className="service mt-10">
+                                        <img src={right} alt='' />
                                         <div className="ml-3  text-start">
                                             <h1 className='text-[#1D3A59]'>Adipiscing eli sed eiusmod</h1>
                                         </div>
-                                    </span>
-                                </div>
-                                <div className="service">
-                                    <svg
-                                        viewBox="0 0 24 24"
-                                        style={{
-                                            width: "1.2000000000000002rem",
-                                            display: "inline-block",
-                                            height: "1.2000000000000002rem",
-                                        }}
-                                        role="presentation"
-                                        className=" service2"
-                                        data-align="middle"
-                                    >
-                                        <path
-                                            d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
-                                            style={{ fill: "#FF7400" }}
-                                        ></path>
-                                    </svg>
-                                    <span>
-                                        <div className="ml-3  text-start">
-                                            <h1 className='text-[#1D3A59]'>Tempor incididunt</h1>
-                                        </div>
-                                    </span>
-                                </div>
-                                <div className="service pb-3">
-                                    <svg
-                                        viewBox="0 0 24 24"
-                                        style={{
-                                            width: "1.2000000000000002rem",
-                                            display: "inline-block",
-                                            height: "1.2000000000000002rem",
-                                        }}
-                                        role="presentation"
-                                        className=" service2"
-                                        data-align="middle"
-                                    >
-                                        <path
-                                            d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"
-                                            style={{ fill: "#FF7400" }}
-                                        ></path>
-                                    </svg>
-                                    <span>
+                                    </div>
+                                    <div className="service">
+                                        <img src={right} alt='' />
                                         <div className="ml-3  text-start">
                                             <h1 className='text-[#1D3A59]'>Labore et dolore magna</h1>
                                         </div>
-                                    </span>
+                                    </div>
+                                    <div className="service pb-3">
+                                        <img src={right} alt='' />
+                                        <div className="ml-3  text-start">
+                                            <h1 className='text-[#1D3A59]'>Labore et dolore magna</h1>
+                                        </div>
+                                    </div>
                                 </div>
-
                                 <button
                                     type="button"
                                     className="btnn"
