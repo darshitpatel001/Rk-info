ad1d6bbb9041ceedd72edcc6bc4cbc00e12c5163
darshitpatel <darshitakbari9999@gmail.com>
2023-07-05 22:35:42 -0700

    dc

--- rk-info/src/component/home/Social.js
+++ rk-info/src/component/home/Social.js
@@ -11,37 +11,33 @@ export default function Social() {
             <section className="services">
                 <div className="container text-center pt-20 pb-16">
                     <div className=" grid xl:grid-cols-3 lg:grid-cols-3 md:grid-1 py-2 mb-2 imgss wow animate__flipInX" data-wow-duration="1s">
-                        
                         <div>
                             <img src={Social1} alt="" className='social' />
                             <div>
                                 <h1 className='social1'>Online Shop</h1>
                                 <h2 className='social2'>You can find new and second-hand
                                     gadgets at our online shop.</h2>
-                                <a href='/product'><img src={Social4} alt=""  className='social' /></a>
+                                <a href='/product'><img src={Social4} alt="" className='social' /></a>
                             </div>
                         </div>
-
                         <div>
                             <img src={Social2} alt="" className='social' />
                             <div>
                                 <h1 className='social1'>Locations</h1>
                                 <h2 className='social2'>Find a repair shop near you. View
                                     more contact information.</h2>
-                                    <a href='/contact'><img src={Social4} alt=""  className='social' /></a>
+                                <a href='/contact'><img src={Social4} alt="" className='social' /></a>
                             </div>
                         </div>
-
                         <div>
                             <img src={Social3} alt="" className='social' />
                             <div>
                                 <h1 className='social1'>Support</h1>
                                 <h2 className='social2'>We offer a 24/7 customer hotline<br />
                                     so we can help you online.</h2>
-                                    <a href='/contact'><img src={Social4} alt="" className='social' /></a>
+                                <a href='/contact'><img src={Social4} alt="" className='social' /></a>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </section>
