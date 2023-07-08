755bac5646cbd2eeb9dce992b16ee1a2e3af9fa4
darshitpatel <darshitakbari9999@gmail.com>
2023-07-08 13:55:05 +0530

    cxc

--- rk-info/src/component/home/Repair.js
+++ rk-info/src/component/home/Repair.js
@@ -25,7 +25,7 @@ export default function Repair() {
             <div className="col-lg-3 col-md-6 col-sm-6 col-12 portfolio-item filter-web">
               <img src={services2}  alt=''className="img-fluid imgss" />
               <div className="portfolio-info">
-                <h4 className='repair1'>CCTV Cable</h4>
+                <h4 className='repair1'>Power Supply</h4>
                 <p className='repair2'>selling product</p>
               </div>
             </div>
@@ -33,7 +33,7 @@ export default function Repair() {
             <div className="col-lg-3 col-md-6 col-sm-6 col-12 portfolio-item filter-app">
               <img src={services3} alt='' className="img-fluid imgss" />
               <div className="portfolio-info">
-                <h4 className='repair1'>CCTV Cable</h4>
+                <h4 className='repair1'>Computer System</h4>
                 <p className='repair2'>selling product</p>
               </div>
             </div>
@@ -41,7 +41,7 @@ export default function Repair() {
             <div className="col-lg-3 col-md-6 col-sm-6 col-12 portfolio-item filter-card">
               <img src={services4} alt='' className="img-fluid imgss" />
               <div className="portfolio-info">
-                <h4 className='repair1'>CCTV Cable</h4>
+                <h4 className='repair1'>4G SIM Router</h4>
                 <p className='repair2'>selling product</p>
               </div>
             </div>
