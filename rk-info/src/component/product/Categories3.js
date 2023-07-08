461e9e3f2477967dbd1609dd6a2e01492e655831
darshitpatel <darshitakbari9999@gmail.com>
2023-07-08 12:30:05 +0530

    fgcdfvbg

--- rk-info/src/component/product/Categories3.js
+++ rk-info/src/component/product/Categories3.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Categorie1 from '../assets/img/fire.png'
 import Categorie2 from '../assets/img/Wireless.png'
 import Categorie3 from '../assets/img/Biometric Solution.png'
-import Categorie4 from '../assets/img/Epabx.png'
+import Categorie4 from '../assets/img/Home Automation.png'
+import Categorie5 from '../assets/img/Networking.png'
+import Categorie6 from '../assets/img/Smart Drawer Lock.png'
+import Categorie7 from '../assets/img/Computer Peripherals.png'
+import Categorie8 from '../assets/img/Access Control Solution.png'
 
 export default function Categories3() {
   return (
@@ -30,7 +34,35 @@ export default function Categories3() {
           <div class="col-md-6 col-lg-3 Categories_item pb-10">
             <img src={Categorie4} className="img-fluid imgsss " alt="" />
             <div className='Categories'>
-              <h1>EPABX Telephone Solution</h1>
+              <h1>Home Automation</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container wow animate__backInUp">
+        <div class="row pt-24">
+          <div class="col-md-6 col-lg-3 Categories_item pb-10">
+            <img src={Categorie5} className="img-fluid imgsss " alt="" />
+            <div className='Categories'>
+              <h1>Networking &  IT Solution</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 Categories_item pb-10">
+            <img src={Categorie6} className="img-fluid imgsss " alt="" />
+            <div className='Categories'>
+              <h1>Smart Drawer Lock</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 Categories_item pb-10">
+            <img src={Categorie7} className="img-fluid imgsss " alt="" />
+            <div className='Categories'>
+              <h1>Computer Peripherals & Accessories</h1>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3 Categories_item pb-10">
+            <img src={Categorie8} className="img-fluid imgsss " alt="" />
+            <div className='Categories'>
+              <h1>Access Control Solution</h1>
             </div>
           </div>
         </div>
